fe9de0100be81eafbc234dac3a5354ee86b3e1f4
Teo Pinillo <teopinillo@gmail.com>
2020-08-26 02:55:51 +0100

    added users app

--- newspaper_project/users/admin.py
+++ newspaper_project/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm
+from .forms import CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin (UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+admin.site.register (CustomUser, CustomUserAdmin)
+
+#need to:
+#python manage.py makenigrations users
+#python manage.py migrate
